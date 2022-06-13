80d2593e501f7188c9fd9390d016ec7447fb30f6
jwgatt <jonnyliggatt@gmail.com>
2022-06-13 21:44:44 +0200

    button state and css

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { bankOne } from './constants.js';
 import './App.scss'
 
 function App() {
   return (
-    <div className='body'>
+    <div className='inner-container'>
       <h2>drum machine</h2>
 
       {/* Map the sounds in the imported dictionary to the buttons that we will render*/}
@@ -14,17 +14,41 @@ function App() {
 }
 
 const Pad = ({ clip }) => {
+
+  // Active state for button animation
+  const [active, setActive] = React.useState(false)
+
+  // UseEffect to handle key presses
+  React.useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    }
+  }, [])
+
+  // If keyCode matches the keyCode mapped to clip, play the clip
+  const handleKeyPress = (e) => {
+    if (e.keyCode === clip.keyCode) {
+      playSound();
+    }
+  }
+
   const playSound = () => {
     const bankOne = document.getElementById(clip.keyTrigger)
+    setActive(true);
+    setTimeout(() => setActive(false), 200)
 
     // Set the audio to start from the beginning
     bankOne.currentTime = 0
     bankOne.play()
-  }
+  };
+
   return (
-    <div onClick={playSound} className='inner-container'>
-      <audio className='clip' id={clip.keyTrigger} src={clip.url} />
-      {clip.keyTrigger}
+    <div className='pad-bank'>
+      <div onClick={playSound} className={` drum-pad  ${active && 'inner-select'}`}>
+        <audio className='clip' id={clip.keyTrigger} src={clip.url} />
+        {clip.keyTrigger}
+      </div>
     </div>
   )
 }
